dfa0b7abdacd849d7d4e177be88466ccf2e03e37
James Rennie <21192547+JimJamJamie@users.noreply.github.com>
2021-04-27 22:36:38 +0100

    Removed filename comment and logic gate variables
    The filename header comment seems pointless on reflection. Underscores in the logic gate variable names makes them much easier to read

--- binaryadder.py
+++ binaryadder.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-'''
-binaryadder.py
-'''
-
 class BinaryAdder():
 
     def and_gate(self, a0, b0):
@@ -37,29 +33,29 @@ class BinaryAdder():
         return a0, b0, c0
 
     def logic_gates(self, a0, b0):
-        a0andb0 = self.and_gate(a0, b0)
-        a0orb0 = self.or_gate(a0, b0)
-        a0xorb0 = self.xor_gate(a0, b0)
+        a0_and_b0 = self.and_gate(a0, b0)
+        a0_or_b0 = self.or_gate(a0, b0)
+        a0_xor_b0 = self.xor_gate(a0, b0)
 
-        return a0andb0, a0orb0, a0xorb0
+        return a0_and_b0, a0_or_b0, a0_xor_b0
 
-    def show_logic_gates(self, a0, b0, a0andb0, a0orb0, a0xorb0):
+    def show_logic_gates(self, a0, b0, a0_and_b0, a0_or_b0, a0_xor_b0):
         print('\n' + '-' * 38)
-        print('Logic gate ' + str(a0) + ' AND ' + str(b0) + ': ' + str(a0andb0))
-        print('Logic gate ' + str(a0) + ' OR  ' + str(b0) + ': ' + str(a0orb0))
-        print('Logic gate ' + str(a0) + ' XOR ' + str(b0) + ': ' + str(a0xorb0))
+        print('Logic gate ' + str(a0) + ' AND ' + str(b0) + ': ' + str(a0_and_b0))
+        print('Logic gate ' + str(a0) + ' OR  ' + str(b0) + ': ' + str(a0_or_b0))
+        print('Logic gate ' + str(a0) + ' XOR ' + str(b0) + ': ' + str(a0_xor_b0))
         print('-' * 38)
 
-    def calculate_result(self, a0, b0, c0, a0xorb0):
-        suma0b0 = self.xor_gate(a0xorb0, c0)
-        c1 = self.and_gate(a0xorb0, c0)
+    def calculate_result(self, a0, b0, c0, a0_xor_b0):
+        sum_a0_b0 = self.xor_gate(a0_xor_b0, c0)
+        c1 = self.and_gate(a0_xor_b0, c0)
         c2 = self.and_gate(a0, b0)
         carry_bit = self.or_gate(c1, c2)
-        result = str(carry_bit) + str(suma0b0)
+        result = str(carry_bit) + str(sum_a0_b0)
 
-        return suma0b0, c1, c2, carry_bit, result
+        return sum_a0_b0, c1, c2, carry_bit, result
 
-    def show_result(self, a0, b0, c0, c1, c2, a0xorb0, suma0b0, carry_bit, result):
+    def show_result(self, a0, b0, c0, c1, c2, a0_xor_b0, sum_a0_b0, carry_bit, result):
         print('\nThe addition calculation is: '+ str(a0) + ' PLUS ' + str(b0) +  ', CARRY ' + str(c0) + '\n')
 
         print('Initial state of adder circuit:')
@@ -68,12 +64,12 @@ class BinaryAdder():
         print('C (CARRY): ' + str(c0))
 
         print('\nCalculate the sum of A and B:')
-        print('A XOR B [' + str(a0) + ' + ' + str(b0) + ']: ' + str(a0xorb0))
+        print('A XOR B [' + str(a0) + ' + ' + str(b0) + ']: ' + str(a0_xor_b0))
         print('\nFactor in the previous carry bit:')
-        print('SUM = (A XOR B) XOR C [' + str(a0xorb0) +  ' + ' + str(c0) + ']: ' + str(suma0b0))
+        print('SUM = (A XOR B) XOR C [' + str(a0_xor_b0) +  ' + ' + str(c0) + ']: ' + str(sum_a0_b0))
 
         print('\nGenerate the input values for carry bit calculation:')
-        print('C1 = (A XOR B) AND C [' + str(a0xorb0) + ' + ' + str(c0) + ']: ' + str(c1))
+        print('C1 = (A XOR B) AND C [' + str(a0_xor_b0) + ' + ' + str(c0) + ']: ' + str(c1))
         print('C2 = A AND B [' + str(a0) + ' + ' + str(b0) + ']: ' + str(c2))
 
         print('\nCalculate the carry bit')
@@ -90,10 +86,10 @@ def main():
     adder = BinaryAdder()
     adder.show_intro()
     a0, b0, c0 = adder.get_user_input()
-    a0andb0, a0orb0, a0xorb0 = adder.logic_gates(a0, b0)
-    adder.show_logic_gates(a0, b0, a0andb0, a0orb0, a0xorb0)
-    suma0b0, c1, c2, carry_bit, result = adder.calculate_result(a0, b0, c0, a0xorb0)
-    adder.show_result(a0, b0, c0, c1, c2, a0xorb0, suma0b0, carry_bit, result)
+    a0_and_b0, a0_or_b0, a0_xor_b0 = adder.logic_gates(a0, b0)
+    adder.show_logic_gates(a0, b0, a0_and_b0, a0_or_b0, a0_xor_b0)
+    sum_a0_b0, c1, c2, carry_bit, result = adder.calculate_result(a0, b0, c0, a0_xor_b0)
+    adder.show_result(a0, b0, c0, c1, c2, a0_xor_b0, sum_a0_b0, carry_bit, result)
     adder.quit()
 
 if __name__ == '__main__':
